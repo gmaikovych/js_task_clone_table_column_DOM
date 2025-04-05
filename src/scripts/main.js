23ba1465280bf41fca924b96702b0413d05cdc54
Galyna Maikovych <gmaik.com@gmail.com>
2025-04-05 15:50:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+function cloneColumn() {
+  const rows = document.querySelectorAll('tr');
+
+  for (let i = 0; i < rows.length; i++) {
+    const cell = rows[i].cells[1];
+    const cellCopy = cell.cloneNode(true);
+
+    rows[i].append(cellCopy);
+  }
+
+  for (let i = 0; i < rows.length; i++) {
+    const cell = rows[i].cells[4];
+
+    rows[i].append(cell);
+  }
+}
+
+cloneColumn();
