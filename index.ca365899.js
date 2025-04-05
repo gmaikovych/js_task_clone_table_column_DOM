47d92bd3fef0df029d0b1e62c9f4b48013d16b77
Galyna Maikovych <gmaik.com@gmail.com>
2025-04-05 15:56:21 +0300

    make build

---
+++ index.ca365899.js
@@ -0,0 +1,2 @@
+!function(){for(var e=document.querySelectorAll("tr"),l=0;l<e.length;l++){var r=e[l].cells[1].cloneNode(!0);e[l].insertBefore(r,e[l].cells[4])}}();
+//# sourceMappingURL=index.ca365899.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+function cloneColumn() {
+    const rows = document.querySelectorAll("tr");
+    for(let i = 0; i < rows.length; i++){
+        const cell = rows[i].cells[1];
+        const cellCopy = cell.cloneNode(true);
+        rows[i].insertBefore(cellCopy, rows[i].cells[4]);
+    }
+}
+cloneColumn();
+
+//# sourceMappingURL=index.f75de5e1.js.map
